dccc59fb2c6bd80fb272fd5389c65c8781a4f691
mosesmuema <muemamose03@gmail.com>
2022-09-16 15:23:30 -0700

    debugging task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,6 +9,7 @@ int main(void)
 
 	printf("Infinite loop incoming :(\n");
 	i = 0;
+
 	/**
 	*while (i < 10)
 	* putchar(i);
@@ -18,5 +19,6 @@ int main(void)
 		putchar(i);
 	}
 	printf("Infinite loop avoided! \\o/\n");
+
 	return (0);
 }
