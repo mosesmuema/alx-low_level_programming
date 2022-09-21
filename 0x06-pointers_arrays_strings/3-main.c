7ad4afc056e73c41e8792a57324f496d9714c26f
mosesmuema <muemamose03@gmail.com>
2022-09-21 13:24:09 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-main.c
+++ 0x06-pointers_arrays_strings/3-main.c
@@ -6,10 +6,10 @@
 * Return: Always 0.
 */
 int main(void)
-
+{
 	char s1[] = "Hello";
-	char s2[] = "World!";
 
+	char s2[] = "World!";
 	printf("%d\n", _strcmp(s1, s2));
 	printf("%d\n", _strcmp(s2, s1));
 	printf("%d\n", _strcmp(s1, s1));
