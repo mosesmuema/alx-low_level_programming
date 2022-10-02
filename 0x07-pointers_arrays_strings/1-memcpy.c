d3026e7dd0490f0f0e8db72e270b30dccee98850
mosesmuema <muemamose03@gmail.com>
2022-10-02 12:17:13 -0700

    memspy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*_memcpy - copies memory area
+*@src:source
+*@dest:destination *@src:source
+*@n:no. of bytes
+*Return:pointer to dest
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+
+	for (i = 0; n > 0; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
