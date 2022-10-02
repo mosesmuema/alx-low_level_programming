571d02da621c92ab32cbeb7781448ff8b11efc75
mosesmuema <muemamose03@gmail.com>
2022-10-02 12:42:09 -0700

    strspn

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+unsigned int _strspn(char *s, char *accept);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 char *_memset(char *s, char b, unsigned int n);
