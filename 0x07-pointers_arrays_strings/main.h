0dca860736f49244a69f59624f82aa977cf34050
mosesmuema <muemamose03@gmail.com>
2022-10-02 13:21:05 -0700

    set string

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
 int _putchar(char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
