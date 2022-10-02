86f76f7d65f87749a1f457268981132f930534e5
mosesmuema <muemamose03@gmail.com>
2022-10-02 13:03:13 -0700

    chessboard

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
+int _putchar(char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
 char *_strpbrk(char *s, char *accept);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
