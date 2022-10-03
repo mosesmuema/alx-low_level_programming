72d3f6c4483777189cda926471fa2ec435fb0dd2
mosesmuema <muemamose03@gmail.com>
2022-10-02 22:55:18 -0700

    prints out a string

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+*_putchar - prints out output
+*@c : character to e output
+*
+*Return : 1 on success
+*-1 on failure
+*/
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
