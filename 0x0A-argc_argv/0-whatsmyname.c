61bca1b85e1b4023d7203c73a5182e40412679b8
mosesmuema <muemamose03@gmail.com>
2022-10-05 11:17:34 -0700

    prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main-gets code
+*@argc:argument count
+*@argv:command line argument vector
+*Return:always (0)success
+*/
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
