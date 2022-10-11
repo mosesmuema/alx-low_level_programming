3409b7b547f2c3c8c0fdc93ab8a17d14da4d8ba7
mosesmuema <muemamose03@gmail.com>
2022-10-11 02:46:03 -0700

    structures in use

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#iclude "dog.h"
+/**
+*init_dog-initializes structure
+*@d:pointer to structure
+*@name:name of dog
+*@age:age of dog
+*@owner:owner of dog
+*Return:void
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
